21457be2da58af30a8d42c58fb8a7ceacdcc8d91
JasonPathways <jason@pathwaystoinclusiveeducation.com>
2026-08-16 11:26:40 -0400

    Cache Plan My Day feature

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='carolina-curiosity-v7';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg','./data1.js','./data2.js','./data3.js','./data4.js','./data5.js','./data6.js','./data7.js','./data8.js','./data9.js','./data10.js'];
+const CACHE='carolina-curiosity-v8';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./planner.js','./manifest.webmanifest','./icon.svg','./data1.js','./data2.js','./data3.js','./data4.js','./data5.js','./data6.js','./data7.js','./data8.js','./data9.js','./data10.js'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(resp=>{let copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))));
